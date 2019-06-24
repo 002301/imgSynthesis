d4d8bfdbe880408586a3aa503c3dae6070d4955b
002301 <002301@gmail.com>
2019-06-24 17:29:31 +0800

    添加文字竖排功能
    1.0.4  添加文字竖排功能

--- src/imgSynthesis.js
+++ src/imgSynthesis.js
@@ -11,6 +11,7 @@
       @width:Number //文本的宽度
       @size:24 文本字号
       @color:'#000' //颜色
+      @writingMode:'td' //文字竖排显示，默认横排
   @img:Array //合并图片数组
       @x:Number //图片x轴
       @y:Number //图片y轴
@@ -25,7 +26,7 @@ class imgSynthesis{
   async set({width=640,height=1236,txt=null,img}){
     this.canvas.width = width;
     this.canvas.height = height;
-    console.log(txt);
+    // console.log(txt);
       for(let i of img){ 
         let img_bg = new Image();
         img_bg.setAttribute('crossOrigin', 'anonymous');
@@ -50,13 +51,20 @@ class imgSynthesis{
       }
       this.setText();
       for(let i of txt){ 
-        let {text='',size='24px',width=156,color='#000000',x=0,y=0} = i;
+        console.log(i);
+        let {text=null,size='24px',width=156,color='#000000',x=0,y=0,writingMode=''} = i;
         this.ctx.font = size+" bold Arial";
         this.ctx.textAlign = "center";
         this.ctx.textBaseline = "bottom";
         this.ctx.fillStyle = color;
-        let MeasureWrapTextHeight = this.ctx.MeasureWrapTextHeight(text,width,size);
-        this.ctx.wrapText(text,x,y,width,size);
+        // console.log(writingMode);
+        if(writingMode=='td'){
+          this.ctx.fillTextVertical(String(text), x, y);
+        }else{
+          let MeasureWrapTextHeight = this.ctx.MeasureWrapTextHeight(text,width,size);
+          this.ctx.wrapText(String(text),x,y,width,size);
+        }
+        
       }
       return this.canvas.toDataURL();
     };
@@ -137,6 +145,67 @@ class imgSynthesis{
       }
       return strHeight;
     };
+
+  
+
+  /**
+   * 文本竖排显示
+  * @author zhangxinxu(.com)
+  * @licence MIT
+  * @description http://www.zhangxinxu.com/wordpress/?p=7362
+  */
+  CanvasRenderingContext2D.prototype.fillTextVertical = function (text, x, y) {
+    let context = this;
+    let canvas = context.canvas;
+    
+    let arrText = text.split('');
+    let arrWidth = arrText.map(function (letter) {
+        return context.measureText(letter).width;
+    });
+    
+    let align = context.textAlign;
+    let baseline = context.textBaseline;
+    
+    if (align == 'left') {
+        x = x + Math.max.apply(null, arrWidth) / 2;
+    } else if (align == 'right') {
+        x = x - Math.max.apply(null, arrWidth) / 2;
+    }
+    if (baseline == 'bottom' || baseline == 'alphabetic' || baseline == 'ideographic') {
+        y = y - arrWidth[0] / 2;
+    } else if (baseline == 'top' || baseline == 'hanging') {
+        y = y + arrWidth[0] / 2;
+    }
+    
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+    
+    // 开始逐字绘制
+    arrText.forEach(function (letter, index) {
+        // 确定下一个字符的纵坐标位置
+        let letterWidth = arrWidth[index];
+        // 是否需要旋转判断
+        let code = letter.charCodeAt(0);
+        if (code <= 256) {
+            context.translate(x, y);
+            // 英文字符，旋转90°
+            context.rotate(90 * Math.PI / 180);
+            context.translate(-x, -y);
+        } else if (index > 0 && text.charCodeAt(index - 1) < 256) {
+            // y修正
+            y = y + arrWidth[index - 1] / 2;
+        }
+        context.fillText(letter, x, y);
+        // 旋转坐标系还原成初始态
+        context.setTransform(1, 0, 0, 1, 0, 0);
+        // 确定下一个字符的纵坐标位置
+        let letterWidth2 = arrWidth[index];
+        y = y + letterWidth2;
+    });
+    // 水平垂直对齐方式还原
+    context.textAlign = align;
+    context.textBaseline = baseline;
+  };
   }
 }
 module.exports  = imgSynthesis;
