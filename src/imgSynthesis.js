a77f87cf97ad56023a00d9e5777c24796f4040c7
ma_ning <ma_ning@corp.netease.com>
2020-04-07 17:07:31 +0800

    添加单独提交方法
    修改set函数为init
    添加addTxt()方法，可以添加一个文本；
    添加addImg()方法，可以添加一个图片；
    添加getImg()方法，可以得到合成的图片；

--- src/imgSynthesis.js
+++ src/imgSynthesis.js
@@ -18,60 +18,106 @@
       @src:String //图片地址(必填)
 */
 
-class imgSynthesis{
-  constructor(){
+class imgSynthesis {
+  constructor() {
     this.canvas = document.createElement("canvas");
     this.ctx = this.canvas.getContext('2d');
   }
-  async set({width=640,height=1236,txt=null,img}){
+  async init({ width = 640, height = 1236,txt = null, img =null}) {
+    // console.log(width, height)
     this.canvas.width = width;
     this.canvas.height = height;
     // console.log(txt);
-      for(let i of img){ 
-        let img_bg = new Image();
-        img_bg.setAttribute('crossOrigin', 'anonymous');
-        await  new Promise((resolve,reject)=>{
-          if(i.src){
-            img_bg.onload = ()=>{
-              let {x=0,y=0} = i;
-              if(i.width!=undefined){
-                this.ctx.drawImage(img_bg,x,y,i.width,i.height);
-              }else{
-                this.ctx.drawImage(img_bg,x,y);
+    if(img!=null){
+      for (let i of img) {
+        await new Promise((resolve, reject) => {
+          let img_bg = new Image();
+          img_bg.setAttribute('crossOrigin', 'anonymous');
+          if (i.src) {
+            img_bg.onload = () => {
+              let { x = 0, y = 0 } = i;
+              if (i.width != undefined) {
+                this.ctx.drawImage(img_bg, x, y, i.width, i.height);
+              } else {
+                this.ctx.drawImage(img_bg, x, y);
               }
-              
+  
               resolve();
             }
-            img_bg.src=i.src;
-          }else{
+            img_bg.src = i.src;
+          } else {
             reject('缺少src参数');
           }
-          
         })
       }
-      this.setText();
-      for(let i of txt){ 
-        console.log(i);
-        let {text=null,size='24px',width=156,color='#000000',x=0,y=0,writingMode=''} = i;
-        this.ctx.font = size+" bold Arial";
-        this.ctx.textAlign = "center";
-        this.ctx.textBaseline = "bottom";
-        this.ctx.fillStyle = color;
-        // console.log(writingMode);
-        if(writingMode=='td'){
-          this.ctx.fillTextVertical(String(text), x, y);
-        }else{
-          let MeasureWrapTextHeight = this.ctx.MeasureWrapTextHeight(text,width,size);
-          this.ctx.wrapText(String(text),x,y,width,size);
-        }
-        
+    }
+    
+    if (txt != null) {
+      for (let i of txt) {
+        this.addTxt(i);
+
       }
       return this.canvas.toDataURL();
-    };
+    }
+
+  };
+  //添加文本
+  addTxt(i) {
+    this.setText();
+    let { text = null, size = '24px', width = 156, color = '#000000', x = 0, y = 0, writingMode = '', align = 'center' } = i;
+    if (text.length < 5) {
+      size = '35px';
+      y += 20;
+    } else if (text.length < 7) {
+      size = '30px';
+      y += 5;
+    } else if (text.length < 9) {
+
+    } else {
+      size = '20px';
+    }
+    this.ctx.font = size + " bold fz";
+    this.ctx.textAlign = align;
+    this.ctx.textBaseline = "bottom";
+    this.ctx.fillStyle = color;
+    // console.log(writingMode);
+    if (writingMode == 'td') {
+      this.ctx.fillTextVertical(String(text), x, y);
+    } else {
+      let MeasureWrapTextHeight = this.ctx.MeasureWrapTextHeight(text, width, size);
+      this.ctx.wrapText(String(text), x, y, width, size);
+    }
+    console.log('text', text)
+    // return this.canvas.toDataURL();
+  }
+  addImg(i) {
+    new Promise((resolve, reject) => {
+      let img_bg = new Image();
+      img_bg.setAttribute('crossOrigin', 'anonymous');
+      if (i.src) {
+        img_bg.onload = () => {
+          let { x = 0, y = 0 } = i;
+          if (i.width != undefined) {
+            this.ctx.drawImage(img_bg, x, y, i.width, i.height);
+          } else {
+            this.ctx.drawImage(img_bg, x, y);
+          }
+
+          resolve();
+        }
+        img_bg.src = i.src;
+      } else {
+        reject('缺少src参数');
+      }
+    })
+  }
+  getImg() {
+    return this.canvas.toDataURL();
+  }
   //文字设置 kongdejian
-  setText(){
+  setText() {
     // canvas 文字换行
-    CanvasRenderingContext2D.prototype.wrapText = function(
+    CanvasRenderingContext2D.prototype.wrapText = function (
       text,
       x,
       y,
@@ -109,7 +155,7 @@ class imgSynthesis{
       context.fillText(line, x, y);
     };
     // 测量文字高度
-    CanvasRenderingContext2D.prototype.MeasureWrapTextHeight = function(
+    CanvasRenderingContext2D.prototype.MeasureWrapTextHeight = function (
       text,
       maxWidth,
       lineHeight
@@ -146,54 +192,54 @@ class imgSynthesis{
       return strHeight;
     };
 
-  
 
-  /**
-   * 文本竖排显示
-  * @author zhangxinxu(.com)
-  * @licence MIT
-  * @description http://www.zhangxinxu.com/wordpress/?p=7362
-  */
-  CanvasRenderingContext2D.prototype.fillTextVertical = function (text, x, y) {
-    let context = this;
-    let canvas = context.canvas;
-    
-    let arrText = text.split('');
-    let arrWidth = arrText.map(function (letter) {
+
+    /**
+     * 文本竖排显示
+    * @author zhangxinxu(.com)
+    * @licence MIT
+    * @description http://www.zhangxinxu.com/wordpress/?p=7362
+    */
+    CanvasRenderingContext2D.prototype.fillTextVertical = function (text, x, y) {
+      let context = this;
+      let canvas = context.canvas;
+
+      let arrText = text.split('');
+      let arrWidth = arrText.map(function (letter) {
         return context.measureText(letter).width;
-    });
-    
-    let align = context.textAlign;
-    let baseline = context.textBaseline;
-    
-    if (align == 'left') {
+      });
+
+      let align = context.textAlign;
+      let baseline = context.textBaseline;
+
+      if (align == 'left') {
         x = x + Math.max.apply(null, arrWidth) / 2;
-    } else if (align == 'right') {
+      } else if (align == 'right') {
         x = x - Math.max.apply(null, arrWidth) / 2;
-    }
-    if (baseline == 'bottom' || baseline == 'alphabetic' || baseline == 'ideographic') {
+      }
+      if (baseline == 'bottom' || baseline == 'alphabetic' || baseline == 'ideographic') {
         y = y - arrWidth[0] / 2;
-    } else if (baseline == 'top' || baseline == 'hanging') {
+      } else if (baseline == 'top' || baseline == 'hanging') {
         y = y + arrWidth[0] / 2;
-    }
-    
-    context.textAlign = 'center';
-    context.textBaseline = 'middle';
-    
-    // 开始逐字绘制
-    arrText.forEach(function (letter, index) {
+      }
+
+      context.textAlign = 'center';
+      context.textBaseline = 'middle';
+
+      // 开始逐字绘制
+      arrText.forEach(function (letter, index) {
         // 确定下一个字符的纵坐标位置
         let letterWidth = arrWidth[index];
         // 是否需要旋转判断
         let code = letter.charCodeAt(0);
         if (code <= 256) {
-            context.translate(x, y);
-            // 英文字符，旋转90°
-            context.rotate(90 * Math.PI / 180);
-            context.translate(-x, -y);
+          context.translate(x, y);
+          // 英文字符，旋转90°
+          context.rotate(90 * Math.PI / 180);
+          context.translate(-x, -y);
         } else if (index > 0 && text.charCodeAt(index - 1) < 256) {
-            // y修正
-            y = y + arrWidth[index - 1] / 2;
+          // y修正
+          y = y + arrWidth[index - 1] / 2;
         }
         context.fillText(letter, x, y);
         // 旋转坐标系还原成初始态
@@ -201,11 +247,11 @@ class imgSynthesis{
         // 确定下一个字符的纵坐标位置
         let letterWidth2 = arrWidth[index];
         y = y + letterWidth2;
-    });
-    // 水平垂直对齐方式还原
-    context.textAlign = align;
-    context.textBaseline = baseline;
-  };
+      });
+      // 水平垂直对齐方式还原
+      context.textAlign = align;
+      context.textBaseline = baseline;
+    };
   }
 }
-module.exports  = imgSynthesis;
+module.exports = imgSynthesis;
